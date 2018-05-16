057de5b3794a01e027119c6b1fa0950dabbd8f4f
Vinicius Besch Caetano <viniciusbeschcaetano@gmail.com>
2018-05-16 20:35:29 -0300

    Funcao de adicionar aluno refinada para validar entrada e colocar no array usando o metodo push

--- estatisca.js
+++ estatisca.js
@@ -1,14 +1,46 @@
 var alunos = [];
-var numAlunos = 0;
+
+var inNome = document.getElementById("inNome");
+var inCurso = document.getElementById("inCurso");
+var inIdade = document.getElementById("inIdade");
 
 function adicionarAluno() {
-    var nome = document.getElementById("inNome").value;
-    var curso = document.getElementById("inCurso").value;
-    var idade = Number(document.getElementById("inIdade").value);
-    
+    var nome = inNome.value;
+    var curso = inCurso.value;
+    var idade = Number(inIdade.value);
+
+    if (nome == "") {
+        alert("Informe corretamente o nome");
+        inNome.focus();
+        return;
+    }
+
+    if(curso == "") {
+        alert("Informe corretamente o curso");
+        inCurso.focus();
+        return;
+    }
+
+    if(idade <=0) {
+        alert("Informe corretamente a idade");
+        inIdade.focus();
+        return;
+    }
+
     var aluno = {nome: nome, curso: curso, idade: idade}
-    alunos[numAlunos] = aluno;
-    numAlunos++;
+    alunos.push(aluno);
+
+    inNome.value= "";
+    inCurso.value = "";
+    inIdade.value = "";
+
+    inNome.focus();
+
+    alert("Aluno " + alunos[alunos.length -1].nome + " adicionado com sucesso");
+}
+
+function getTotalAlunos() {
+    return alunos.length;
 }
 
 var btAdicionar = document.getElementById("btAdicionar");
